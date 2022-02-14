e1ca33a5359c75894c9065c313c51708930dd181
André dos Santos <adesantosa@gmail.com>
2022-02-14 16:22:02 -0300

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to padawan-sample
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/padawan_sample/core_test.clj
@@ -0,0 +1,7 @@
+(ns padawan-sample.core-test
+  (:require [clojure.test :refer :all]
+            [padawan-sample.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
